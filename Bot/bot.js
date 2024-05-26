f55fadcf0978c0868eac41ce994ea88c6e7eaeb0
Pyvovarchuk <andriy.pyvovarchuk@gmail.com>
2024-05-26 12:56:31 +0300

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -4,7 +4,7 @@ const TOKEN = '7137435593:AAHgs83gYyb1oVKxZBlvik85TUkRm88utHk'
 
 const bot = new Telegraf(TOKEN)
 
-const web_app_link = 'https://telegram-web-4gjbhnu7k-andriys-projects-088dca2f.vercel.app/'
+const web_app_link = 'https://xmtigra.github.io/telegram-web-app'
 
 bot.start((ctx) => ctx.reply('Welcome!', {
   reply_markup: {
